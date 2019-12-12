7255a604c1cd7be6ff80d9902574108bb551d04b
Spraynard <kellan.martin@gmail.com>
2019-12-12 02:27:20 -0500

    Updating app as our container component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import "./css/normalize.css";
+import './css/App.css';
+import ActivityDisplay from "./ActivityDisplay";
+import ActivityGrid from "./ActivityGrid";
+import { IActivity, IActivityDictionary } from './interfaces';
+import ActivityForm from './ActivityForm';
+import ActivityDisplayItem from './ActivityDisplayItem';
+import { IActivityFactory } from "./factories";
+import { getActivitiesInDisplayOrder } from "./helpers";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ selectedActivity, setSelectedActivity ] = React.useState<string | null>(null);
+	const [ activitiesData, setActivitiesData ] = React.useState<IActivityDictionary>({});
+	const [ activities, setActivities ] = React.useState<string[]>([]);
+	const [ timers, setTimers ] = React.useState<object>({})
+	const [ activityFormDescription, setActivityFormDescription ] = React.useState<string>("");
+
+	const handleActivityFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		let new_activity = IActivityFactory(activityFormDescription);
+
+		setSelectedActivity(new_activity.id);
+		setActivities([ ...activities, new_activity.id ])
+		setActivitiesData({ ...activitiesData, [ new_activity.id ] : new_activity })
+
+		setActivityFormDescription("")
+	}
+
+	return (
+		<div className="app">
+			<main className="app-flex-container">
+				<div className="activity-display app-container-item">
+					<ActivityForm handleSubmit={handleActivityFormSubmit} activityFormDescription={activityFormDescription} setActivityFormDescription={setActivityFormDescription}/>
+					<ActivityDisplayItem selectedActivity={(selectedActivity) ?  activitiesData[selectedActivity] : null}/>
+				</div>
+				<ActivityGrid displayActivities={getActivitiesInDisplayOrder(activities, activitiesData)} setSelectedActivity={setSelectedActivity} headers={["Start Time", "End Time", "Duration", "Description"]}/>
+			</main>
+		</div>
+	);
 }
 
 export default App;
