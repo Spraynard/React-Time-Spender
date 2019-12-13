56fcfe2e450185c3c3864d36b3fbf0009377998e
Spraynard <kellan.martin@gmail.com>
2019-12-13 04:27:09 -0500

    factories now using a moment duration object for duration

--- src/domain/factories.ts
+++ src/domain/factories.ts
@@ -1,11 +1,12 @@
 import { IActivity } from "./interfaces";
 import { v4 } from "uuid";
+import moment from "moment";
 
 export const IActivityFactory = ( description: string ): IActivity => ({
     id: v4(),
     description : description,
     startDate : new Date(),
     endDate : null,
-    duration : 0,
+    duration : moment.duration(),
     timerID : null
 });
