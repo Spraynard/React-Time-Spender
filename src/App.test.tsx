a81e43a453467b6e0febe0c762ca57f7c76d0e6f
Spraynard <kellan.martin@gmail.com>
2019-12-13 04:25:03 -0500

    Adding message to app testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+/**
+ * Maybe if this wasn't just a job proto I would make tests but
+ * as of right now I'd just be
+ * putting too much time into it
+ **/
 // test('renders learn react link', () => {
 //   const { getByText } = render(<App />);
 //   const linkElement = getByText(/learn react/i);
