907bec49960a7fa985436604ac6a126864e9f804
Spraynard <kellan.martin@gmail.com>
2019-12-13 04:41:16 -0500

    Updating hooks.ts documentation

--- src/domain/hooks.ts
+++ src/domain/hooks.ts
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 /**
- * Saves to sessionStorage
+ * Function to encapsulate our saving procedures.
  *
  * @param setter Setter hook
  * @param key Key that we are to save to session storage to
@@ -16,12 +16,17 @@ const updateSessionStorageFromUpdateState = ( setter : ( value : any ) => void, 
 
 
 /**
+ * Extremely quick implementation of a hook that uses session storage to handle state somewhat
+ * like it would regularly be handled.
  *
+ * Not too friendly because requires serializer and unserializer functions.
+ * Could abstract that away through defaults or currying at some point.
  * @param initial Initial value if there is no session state
  * @param key Key to store our session state at for this specific state
  * @param unserializer Function that takes content from Storage and converts it into object to use in application
  * @param serializer Function that takes domain object and converts it into an object acceptable for putting through JSON.stringify
  */
+
 export const useStateFromSessionStorage = ( initial: any, key : string, unserializer : ( value : any ) => any, serializer : ( value : any ) => string ) => {
     let serialized: (string | null) = null;
     let unserialized: any = initial;
